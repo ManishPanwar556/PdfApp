55acc11150c9f51edf75c24a34f8dbff9bd1874c
Manish Panwar <panwarmanish843@gmail.com>
2021-02-15 17:00:39 +0530

    Initial Commit

--- app/src/main/java/com/example/pdfapp/MainActivity.kt
+++ app/src/main/java/com/example/pdfapp/MainActivity.kt
@@ -3,35 +3,53 @@ package com.example.pdfapp
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.pdf.PdfRenderer
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import android.util.DisplayMetrics
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.net.toFile
+import androidx.viewpager2.widget.ViewPager2
+import app.num.numandroidpagecurleffect.PageCurlView
 import com.example.pdfapp.databinding.ActivityMainBinding
 import com.github.barteksc.pdfviewer.PDFView
+import com.wajahatkarim3.easyflipviewpager.BookFlipPageTransformer2
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 
 const val TAG = "MAINACTIVITY"
 
 class MainActivity : AppCompatActivity() {
-    lateinit var pdfView: PDFView
+    lateinit var viewPager2: ViewPager2
     lateinit var binding: ActivityMainBinding
+    lateinit var viewPagerAdapter: MyViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        pdfView = binding.pdfView
+        viewPager2 = binding.viewPager
         if (permissionGranted()) {
             showPdfFromStorage()
         }
+
     }
 
     private fun showPdfFromStorage() {
         val browseStorage = Intent(Intent.ACTION_GET_CONTENT)
         browseStorage.type = "application/pdf"
 //        it only opens those pdf which can be open by openFileDescriptor(uri:String)
-  browseStorage.addCategory(Intent.CATEGORY_OPENABLE)
+        browseStorage.addCategory(Intent.CATEGORY_OPENABLE)
         startActivityForResult(Intent.createChooser(browseStorage, "Select Pdf"), 123)
     }
 
@@ -53,9 +71,52 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun showPdfFromUri(uri: Uri?, pdfView: PDFView) {
-        pdfView.fromUri(uri).defaultPage(0).enableSwipe(true).swipeHorizontal(true).load()
+    lateinit var list: List<Bitmap>
+    private fun showPdfFromUri(uri: Uri?) {
+        if (uri != null) {
+            Toast.makeText(this, "${uri.path}", Toast.LENGTH_SHORT).show()
+            GlobalScope.launch(Dispatchers.Main) {
+                withContext(Dispatchers.IO) {
+                    list = convertPdfToBitmap(uri)
+                }
+                Log.e(TAG,"${list.size}")
+                viewPagerAdapter = MyViewPagerAdapter(list)
+                val bookFlipPageTransformer2 = BookFlipPageTransformer2()
+//                bookFlipPageTransformer2.setEnableScale(true)
+//                bookFlipPageTransformer2.scaleAmountPercent = 10f
+                viewPager2.setPageTransformer(bookFlipPageTransformer2)
+            }
+        }
+    }
 
+    //
+    suspend fun convertPdfToBitmap(uri: Uri?): ArrayList<Bitmap> {
+        val list = arrayListOf<Bitmap>()
+        if (uri != null) {
+            val input =
+                contentResolver.openFileDescriptor(uri, "r")
+            val renderer = PdfRenderer(input!!)
+            val pageCount = renderer.pageCount
+            var count = 0
+            Log.e(TAG, "$pageCount")
+            while (count < pageCount) {
+                val page = renderer.openPage(count)
+                val displayMetrics = DisplayMetrics()
+                val width = displayMetrics.widthPixels / displayMetrics.density
+                val bitmap = Bitmap.createBitmap(
+                    120,
+                    120,
+                    Bitmap.Config.ARGB_8888
+                )
+                page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+                list.add(bitmap)
+                page.close()
+
+                count++
+            }
+            renderer.close()
+        }
+        return list
     }
 
     override fun onRequestPermissionsResult(
@@ -82,7 +143,7 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             123 -> {
                 if (resultCode == RESULT_OK && data != null) {
-                    showPdfFromUri(data.data, pdfView)
+                    showPdfFromUri(data.data)
                 }
             }
         }
---
+++ app/src/main/java/com/example/pdfapp/MyViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.pdfapp
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class MyViewPagerAdapter(private val list:List<Bitmap>):RecyclerView.Adapter<MyViewPagerAdapter.MyViewHolder>() {
+    inner class MyViewHolder(val view: View):RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.viewpager_item,parent,false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val pdfImage=holder.view.findViewById<ImageView>(R.id.pdfImage)
+        Glide.with(holder.view).load(list[position]).into(pdfImage)
+    }
+
+    override fun getItemCount()=list.size
+
+}
